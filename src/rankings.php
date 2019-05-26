72156f78bd473290ea47daff56d72c3008480d82
Mauk <maukmul@gmail.com>
2019-05-26 17:22:12 +0200

    placeholder for rankings page

--- src/rankings.php
+++ src/rankings.php
@@ -11,6 +11,19 @@
 </head>
 
 <body>
+<?php
+//    mysqli_report(MYSQLI_REPORT_ALL);
 
+    $mysqli = new mysqli('localhost', 'mauk', '123', 'PendulumRankingsDB');
+
+    $query = "SELECT * FROM songs ORDER BY average_rank";
+
+    $result = $mysqli->query($query);
+
+    foreach($result as $item) {
+        echo "<h1 style=\"color: white;\">" . $item['name'] . " -- " . $item['album'] . " -- " . $item['average_rank'] . "</h1>";
+    }
+
+?>
 </body>
 </html>
